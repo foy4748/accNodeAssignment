191ae1dbed7b64370b21609cf68558b474b721c9
foy4748 <faisaljfcl@gmail.com>
2023-03-08 13:34:39 +0600

    Added /user/update and /user/delete route

--- Controllers/User.js
+++ Controllers/User.js
@@ -4,6 +4,7 @@ const CRUD = require("../Lib/CRUD");
 const router = express.Router();
 const dataPath = `${global.projectRoot}/data.json`;
 
+// GET - A RANDOM USER
 router.get("/random", (req, res) => {
   CRUD.read(req, res, dataPath, (data) => {
     const idx = Math.floor(Math.random() * data.length);
@@ -11,15 +12,16 @@ router.get("/random", (req, res) => {
   });
 });
 
+// GET - ALL USERS
 router.get("/all", (req, res) => {
   CRUD.read(req, res, dataPath, (readData) => {
     return res.send(readData);
   });
 });
 
+// POST - SAVE A USER
 router.post("/save", (req, res) => {
   CRUD.read(req, res, dataPath, (readData) => {
-    console.log(req.body);
     const newData = [...readData, req.body];
     CRUD.write(req, res, dataPath, newData, (isWritten) => {
       if (isWritten) {
@@ -29,4 +31,34 @@ router.post("/save", (req, res) => {
   });
 });
 
+// PATCH - UPDATE A SINGLE USER
+router.patch("/update", (req, res) => {
+  console.log(req.body);
+  CRUD.read(req, res, dataPath, (readData) => {
+    for (let item of readData) {
+      if (item.id == req.body.id) {
+        for (const prop in req.body) {
+          item[prop] = req.body[prop];
+        }
+      }
+    }
+
+    CRUD.write(req, res, dataPath, readData, (isWritten) => {
+      if (isWritten)
+        res.send({ error: false, message: "Updated Successfully" });
+    });
+  });
+});
+
+// DELETE - A SINGLE USER
+router.delete("/delete", (req, res) => {
+  CRUD.read(req, res, dataPath, (readData) => {
+    const newData = readData.filter((item) => item.id != req.body.id);
+
+    CRUD.write(req, res, dataPath, newData, (isWritten) => {
+      if (isWritten)
+        res.send({ error: false, message: "Deleted Successfully" });
+    });
+  });
+});
 module.exports = router;
--- testApi.py
+++ testApi.py
@@ -1,6 +1,7 @@
 import requests as rq
 
 baseURL = "http://127.0.0.1:3001/user"
+headers = { "Content-Type": "application/json"}
 
 # Testing /user/all route
 
@@ -13,12 +14,30 @@ baseURL = "http://127.0.0.1:3001/user"
 # print(res.text)
 
 # Testing /user/save route
-headers = { "Content-Type": "application/json"}
 
-data = {
-        "id":"06",
-        "name":"Test Data"
+
+# data = {
+#         "id":"06",
+#         "name":"Test Data"
+#         }
+
+# res = rq.post(baseURL + "/save", headers=headers, json=data)
+# print(res.text)
+
+# Testing /user/update route
+# updateData = {
+#         "id":"05",
+#         "name":"Faisal"
+#         }
+
+# res = rq.patch(baseURL + "/update", headers=headers, json=updateData)
+# print(res.text)
+
+    
+# Testing /user/delete route
+deleteData = {
+        "id":"03",
         }
 
-res = rq.post(baseURL + "/save", headers=headers, json=data)
+res = rq.delete(baseURL + "/delete", headers=headers, json=deleteData)
 print(res.text)
